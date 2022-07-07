661ef41e23167b647a31c9e149fe6391eb2101f2
jpal91 <98477057+jpal91@users.noreply.github.com>
2022-07-07 17:52:46 -0400

    added redux

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,3 @@
-// import '../styles/globals.css'
 import React from "react";
 import { CssBaseline } from "@mui/material";
 import { ThemeProvider } from "@mui/material/styles";
@@ -7,15 +6,15 @@ import NavBar from "../components/NavBar";
 import theme from "../styles/theme";
 
 function MyApp({ Component, pageProps }) {
-    return (
-        <React.Fragment>
-            <ThemeProvider theme={theme}>
-                <CssBaseline />
-				<NavBar />
-                <Component {...pageProps} />
-            </ThemeProvider>
-        </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <NavBar />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </React.Fragment>
+  );
 }
 
 export default MyApp;
